626f45e775536cf8df0e470f2d9dc4d1f0de5dff
CesarLucasMartins <cesarlucasmartins1989@gmail.com>
2023-08-22 19:46:48 -0300

    actualizacion

--- Ejercicios/Evidencia8.py
+++ Ejercicios/Evidencia8.py
@@ -10,7 +10,7 @@
 #2. Sumar al ejercicio anterior, una pregunta: Si el número es menor imprimir: ¡Falto un poco,
 #seguí participando! Si es mayor, imprimir: ¡Te pasaste, a seguir intentando!
 
-"""numero = int(input ("ingrese un numero "))
+numero = int(input ("ingrese un numero "))
 
 if numero == 10:
     print("¡Usted ha ganado el sorteo!")
@@ -20,7 +20,7 @@ elif numero < 10:
     print ("¡Falto un poco,seguí participando!")
 
 else:
-    print ("¡Te pasaste, a seguir intentando!")"""
+    print ("¡Te pasaste, a seguir intentando!")
     
 
 
@@ -30,7 +30,7 @@ else:
 #mensaje diferente si es viernes, otro mensaje diferente si es sábado o domingo. Si el día
 #ingresado no es ninguno de esos, imprimir otro mensaje.
 
-"""dia = input("ingrese un dia de la semana ").lower()
+dia = input("ingrese un dia de la semana ").lower()
 
 if dia == "lunes":
     print("Arranca la semana!")
@@ -42,20 +42,20 @@ elif dia == "sabado" or dia == "domingo":
     print("A disfrutar el finde!")
 
 else:
-    print("vamos!,un dia a la vez!")"""
+    print("vamos!,un dia a la vez!")
 
 
 
 #4. Escribir un programa que solicite al usuario una letra y, si es una vocal, muestre el mensaje
 #“es vocal”.
 
-"""letra = input("ingrese una letra ").lower()
+letra = input("ingrese una letra ").lower()
 
 if letra in {"a", "e", "i", "o", "u"}:
     print("la letra es una vocal")
 
 else:
-    print("la letra es una consonante")"""
+    print("la letra es una consonante")
 
 
 
@@ -68,7 +68,7 @@ else:
 #1. Escribir un programa que realice la sumatoria de los números que se quiera hasta ingresar
 #hasta que se ingrese -1.
 
-"""suma = 0 
+suma = 0 
 
 while True:
     numero = float(input ("ingrese un numero o -1 para finalizar "))
@@ -78,14 +78,14 @@ while True:
         break
     suma += numero
 
-print("la sumatoria total es" , suma) """
+print("la sumatoria total es" , suma) 
 
 
 #2. Realizar un algoritmo que pida números (se pedirá por teclado la cantidad de números a
 #introducir). El programa debe informar de cuantos números introducidos son mayores que
 #0, menores que 0 e iguales a 0.
 
-"""cantidad = int(input("introduzca la cantidad de numeros que va a ingresar "))
+cantidad = int(input("introduzca la cantidad de numeros que va a ingresar "))
 
 mayor_a_cero = 0
 menor_a_cero = 0
@@ -98,7 +98,7 @@ for i in range (cantidad):
     menor_a_cero += numeros_introducidos < 0
     igual_a_cero += numeros_introducidos == 0
 
-print(f"cantidad de numeros mayores a cero {mayor_a_cero}, cantidad de numeros menores a cero {menor_a_cero}, cantidad de numeros iguales a cero  {igual_a_cero}") """
+print(f"cantidad de numeros mayores a cero {mayor_a_cero},cantidad de numeros menores a cero {menor_a_cero}, cantidad de numeros iguales a cero  {igual_a_cero}") 
 
 
 #3. Algoritmo que pida caracteres e imprima ‘VOCAL’ si son vocales y ‘NO VOCAL’ en caso
@@ -119,8 +119,21 @@ while True:
 
    
 
-
-
-
 #4. Algoritmo que pida números hasta que se introduzca un cero. Debe imprimir la suma y la
 #media de todos los números introducidos.
+
+suma = 0
+contador = 0
+media = 0
+
+numero = float(input("Introduce un número: "))
+
+while numero != 0:
+    suma = suma + numero 
+    contador += 1
+    media = suma/contador
+    numero = float(input("Introduce un número: "))
+print("Se han introducido", contador, "números que en total han sumado", suma)
+print("La media es", media)
+
+
