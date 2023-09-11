2c9281114971ff55c447b38619c54bfb7d638f13
CesarLucasMartins <cesarlucasmartins1989@gmail.com>
2023-09-11 20:51:53 -0300

    Herencia

--- Ejercicios/EvidenciaHerencia.py
+++ Ejercicios/EvidenciaHerencia.py
@@ -1,35 +1,11 @@
 # Partiendo de la base del ejercicio de CLASES, se pide:
 #  Que la clase Docente, sea herencia de la clase Usuario.
 
-class Usuario:
-    def __init__(self, nombre, apellido, dni, fecha_nacimiento, direccion, localidad, codigo_postal, provincia, telefono_celular, email):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.dni = dni
-        self.fecha_nacimiento = fecha_nacimiento
-        self.direccion = direccion
-        self.localidad = localidad
-        self.codigo_postal = codigo_postal
-        self.provincia = provincia
-        self.telefono_celular = telefono_celular
-        self.email = email
-        
-    
-
-usuario1 = Usuario("Gaston", "Ferretti", "44528956", "20/03/2001", "Chascomus 454", "Bs As", "0245", "Junin", "0263458974", "Tonga@gmail.com")
+from EvidenciaClass import Usuario
 
 class Docente(Usuario):
     def __init__(self, nombre, apellido, dni, fecha_nacimiento, direccion, localidad, codigo_postal, provincia, telefono_celular, email):
         super().__init__(nombre, apellido, dni, fecha_nacimiento, direccion, localidad, codigo_postal, provincia, telefono_celular, email)
-
-docente1 = Docente("Gladis", "Sarabia", "38456925", "20/05/1980", "Quilmes 2415", "Cordoba", "5000", "Cordoba", "01154689526","profe@gmail.com")
-print("información del docente: ")
-print(f"Nombre: {docente1.nombre} {docente1.apellido}")
-print(f"DNI: {docente1.dni}")
-print(f"Email: {docente1.email}")
-print()
-
-
 
 #  Generar una clase nueva que sea compra y contenga:
 # o Id_Compra
@@ -41,14 +17,48 @@ print()
 
 class Compra:
     def __init__(self, Id_Compra, Id_Carrito_Compra, Id_Medios_Pago, Id_Usuario, Fecha, Monto_Total):
-        self.Id_Compra = Id_Compra
-        self.Id_Carrito_Compra = Id_Carrito_Compra
-        self.Id_Medios_Pago = Id_Medios_Pago
-        self.Id_Usuario = Id_Usuario
-        self.Fecha = Fecha
-        self.Monto_Total = Monto_Total
+        self._Id_Compra = Id_Compra
+        self._Id_Carrito_Compra = Id_Carrito_Compra
+        self._Id_Medios_Pago = Id_Medios_Pago
+        self._Id_Usuario = Id_Usuario
+        self._Fecha = Fecha
+        self._Monto_Total = Monto_Total
 
-compra = Compra("323", "013", "222", "123456", "05/09/2023", 200)
+    def get_Id_Compra(self):
+        return self._Id_Compra
+
+    def set_Id_Compra(self, nuevo_Id_Compra):
+        self._Id_Compra = nuevo_Id_Compra
+
+    def get_Id_Carrito_Compra(self):
+        return self._Id_Carrito_Compra
+
+    def set_Id_Carrito_Compra(self, nuevo_Id_Carrito_Compra):
+        self._Id_Carrito_Compra = nuevo_Id_Carrito_Compra
+
+    def get_Id_Medios_Pago(self):
+        return self._Id_Medios_Pago
+
+    def set_Id_Medios_Pago(self, nuevo_Id_Medios_Pago):
+        self._Id_Medios_Pago = nuevo_Id_Medios_Pago
+
+    def get_Id_Usuario(self):
+        return self._Id_Usuario
+
+    def set_Id_Usuario(self, nuevo_Id_Usuario):
+        self._Id_Usuario = nuevo_Id_Usuario
+
+    def get_Fecha(self):
+        return self._Fecha
+
+    def set_Fecha(self, nueva_Fecha):
+        self._Fecha = nueva_Fecha
+
+    def get_Monto_Total(self):
+        return self._Monto_Total
+
+    def set_Monto_Total(self, nuevo_Monto_Total):
+        self._Monto_Total = nuevo_Monto_Total
 
 #  Generar una clase de Medios de Contacto que contenga:
 # o Id_MedioContacto
@@ -60,12 +70,48 @@ compra = Compra("323", "013", "222", "123456", "05/09/2023", 200)
 
 class MediosDeContacto:
     def __init__(self, Id_MedioContacto, Fecha, Email, Telefono, Direccion, Nombre):
-        self.Id_MedioContacto = Id_MedioContacto
-        self.Fecha = Fecha
-        self.Email = Email
-        self.Telefono = Telefono
-        self.Direccion = Direccion
-        self.Nombre = Nombre
+        self._Id_MedioContacto = Id_MedioContacto
+        self._Fecha = Fecha
+        self._Email = Email
+        self._Telefono = Telefono
+        self._Direccion = Direccion
+        self._Nombre = Nombre
+
+    def get_Id_MedioContacto(self):
+        return self._Id_MedioContacto
+
+    def set_Id_MedioContacto(self, nuevo_Id_MedioContacto):
+        self._Id_MedioContacto = nuevo_Id_MedioContacto
+
+    def get_Fecha(self):
+        return self._Fecha
+
+    def set_Fecha(self, nueva_Fecha):
+        self._Fecha = nueva_Fecha
+
+    def get_Email(self):
+        return self._Email
+
+    def set_Email(self, nuevo_Email):
+        self._Email = nuevo_Email
+
+    def get_Telefono(self):
+        return self._Telefono
+
+    def set_Telefono(self, nuevo_Telefono):
+        self._Telefono = nuevo_Telefono
+
+    def get_Direccion(self):
+        return self._Direccion
+
+    def set_Direccion(self, nueva_Direccion):
+        self._Direccion = nueva_Direccion
+
+    def get_Nombre(self):
+        return self._Nombre
+
+    def set_Nombre(self, nuevo_Nombre):
+        self._Nombre = nuevo_Nombre
 
 #  Generar una clase que sea: Tipos de Medio de Contacto en formato enum:
 # o WhatsApp
@@ -78,19 +124,19 @@ class MediosDeContacto:
 class TiposDeMedioDeContacto(MediosDeContacto):
     def __init__(self, Id_MedioContacto, Fecha, Email, Telefono, Direccion, Nombre, Tipo):
         super().__init__(Id_MedioContacto, Fecha, Email, Telefono, Direccion, Nombre)
-        self.Tipo = Tipo
+        self._Tipo = Tipo
+      
+    def get_Tipo(self):
+        return self._Tipo
 
-
-whatsapp_contacto = TiposDeMedioDeContacto("222", "07/07/2020", "", "351456842", "losa 55", "Julia Acosta", "WhatsApp")
-correo_contacto = TiposDeMedioDeContacto("223", "10/10/2021", "Maria@gmail.com", "", "Arias 22", "María Rodríguez", "Correo electrónico")
-call_center_contacto = TiposDeMedioDeContacto("224", "11/8/2022", "", "3512045789", "Lujan 78", "Carlos Rene", "Call center")
-referido_interno_contacto = TiposDeMedioDeContacto("225", "06/12/2023", "", "", "Sociedad b 458", "Ana Gache", "Referido interno")
-
-print("Tipo de Medio de Contacto:", whatsapp_contacto.Tipo)
-print("Tipo de Medio de Contacto:", correo_contacto.Tipo)
-print("Tipo de Medio de Contacto:", call_center_contacto.Tipo)
-print("Tipo de Medio de Contacto:", referido_interno_contacto.Tipo)
-
-
+    def set_Tipo(self, nuevo_Tipo):
+        self._Tipo = nuevo_Tipo
         
-        
+"""compra = Compra("579", "456", "789", "usuario123", "2023-09-11", 500.0)
+
+id_compra = compra.get_Id_Compra()
+print(f"Id de Compra: {id_compra}")
+nuevo_id_compra = "789"
+compra.set_Id_Compra(nuevo_id_compra)
+print(f"Nuevo Id de Compra: {compra.get_Id_Compra()}")"""
+
